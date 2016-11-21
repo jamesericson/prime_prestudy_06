34eff27e59efb919a66180559bebeb95cd14cd45
James Ericson <jamesericson@gmail.com>
2016-11-21 04:16:49 -0600

    added a welcome message

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-console.log("Hey this works!");
+console.log("Hey this works??");
 var clickNo = 0;
 
 
 $(document).ready(function() {
   $('header, main, footer').hide();
   $('body').animate({padding:"-=50"},1);
-  $('.portrait').animate({left: "+=200"},1);
+  $('.portrait').animate({opacity: .20, left: "+=200"},1);
+
+  $('.welcome').on('mouseenter', function() {
+    if(clickNo == 0){
+    $('.portrait').animate({opacity: .50},'fast');
+    }
+  })
+  $('.welcome').on('mouseleave', function() {
+    if(clickNo == 0){
+    $('.portrait').animate({opacity: .20},'fast');
+    }
+  })
 
 
-  $('.portrait').on('click', function() {
+  $('.portrait, .welcome').on('click', function() {
   if (clickNo == 0){
     $('header, main, footer').slideDown();
     $('body').animate({padding:"+=50"});
-    $('.portrait').animate({left: "-=200",});
+    $('.welcome').animate({opacity: 0, left: "-=200"});
+    $('.portrait').animate({opacity: 1, left: "-=200"});
     clickNo++;
     console.log("clickNo++");
   }
   });
-  
+
   console.log("clickNo = "+clickNo);
 });
