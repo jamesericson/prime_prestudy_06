f5643861fd0ae72c560c10842b9ed54301f746e8
James Ericson <jamesericson@gmail.com>
2016-11-21 02:58:09 -0600

    met assignment requirments

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ var clickNo = 0;
 
 
 $(document).ready(function() {
+  $('header, main, footer').hide();
+  $('body').animate({padding:"-=50"},1);
+  $('.portrait').animate({left: "+=200"},1);
+
+
   $('.portrait').on('click', function() {
-    $('header, main, footer').slideToggle();
+  if (clickNo == 0){
+    $('header, main, footer').slideDown();
+    $('body').animate({padding:"+=50"});
+    $('.portrait').animate({left: "-=200",});
+    clickNo++;
+    console.log("clickNo++");
+  }
   });
+  
+  console.log("clickNo = "+clickNo);
 });
